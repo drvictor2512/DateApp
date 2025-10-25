c6370af376c2846a6c2d84c7680f80c25b19aa15
Tran Thang <trvictor2512@gmail.com>
2025-10-25 21:39:39 +0700

    first commit

---
+++ app/tabs/_layout.tsx
@@ -0,0 +1,36 @@
+import { Ionicons } from '@expo/vector-icons';
+import { Tabs } from "expo-router";
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
+export default function TabLayout() {
+    const insets = useSafeAreaInsets();
+    return (
+        <Tabs screenOptions={{
+            headerShown: false, tabBarStyle: {
+                borderTopWidth: 1,
+                paddingTop: 5,
+                paddingBottom: insets.bottom,
+                height: 60 + insets.bottom
+
+            },
+            tabBarShowLabel: false
+        }}>
+            <Tabs.Screen name="home" options={{
+                tabBarIcon: ({ color, size }) => (<Ionicons
+                    name="person-outline" size={size} color={color} />)
+            }} />
+            <Tabs.Screen name="heart" options={{
+                tabBarIcon: ({ color, size }) => (<Ionicons
+                    name="heart-outline" size={size} color={color} />)
+            }} />
+            <Tabs.Screen name="save" options={{
+                tabBarIcon: ({ color, size }) => (<Ionicons
+                    name="bookmark-outline" size={size} color={color} />)
+            }} />
+            <Tabs.Screen name="chat" options={{
+                tabBarIcon: ({ color, size }) => (<Ionicons
+                    name="paper-plane-outline" size={size} color={color} />)
+            }} />
+        </Tabs>
+    )
+}
---
+++ app/tabs/heart.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export default function heart() {
+    return (
+        <View>
+            <Text>heart</Text>
+        </View>
+    )
+}
